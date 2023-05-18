24d5252480cbae53be82ce0a619939e4a252902b
Vaida Aurel <Vaida_Aurel@yahoo.com>
2023-05-18 22:23:45 +0300

    split into SideMenu, KnowLanguagesTable

--- src/App.tsx
+++ src/App.tsx
@@ -57,25 +57,7 @@ function MainMenu() {
 function ContentWrapper() {
   return (
     <section id="content">
-      <div id="side-menu">
-        <section>
-          <h2>Contact Form</h2>
-          <form id="sectione-left-menu">
-            <div id="Email">
-              <label htmlFor="e">Email: </label>
-              <input type="email" id="e" name="e," required placeholder="example@yahoo.com" />
-            </div>
-            <div id="UserName">
-              <label htmlFor="n"> Name: </label>
-              <input type="text" id="Name" name="na" required placeholder="Full Name" />
-            </div>
-
-            <div className="Contact-Us">
-              <button>Contact us</button>
-            </div>
-          </form>
-        </section>
-      </div>
+      <SideMenu />
 
       <div id="main">
         <div id="home" className="page">
@@ -116,27 +98,57 @@ function ContentWrapper() {
 
         <div id="languages" className="page">
           <h2>Known Language</h2>
-          <table border={1}>
-            <tr>
-              <th>Language</th>
-              <th>Level</th>
-            </tr>
-            <tr>
-              <td>Romana</td>
-              <td>Native-Language</td>
-            </tr>
-            <tr>
-              <td>English</td>
-              <td>B2</td>
-            </tr>
-            <tr>
-              <td>Deutschland</td>
-              <td>B2</td>
-            </tr>
-          </table>
+          <KnowLanguagesTable />
         </div>
       </div>
     </section>
+  );
+}
+
+function SideMenu() {
+  return (
+    <div id="side-menu">
+      <section>
+        <h2>Contact Form</h2>
+        <form id="sectione-left-menu">
+          <div id="Email">
+            <label htmlFor="e">Email: </label>
+            <input type="email" id="e" name="e," required placeholder="example@yahoo.com" />
+          </div>
+          <div id="UserName">
+            <label htmlFor="n"> Name: </label>
+            <input type="text" id="Name" name="na" required placeholder="Full Name" />
+          </div>
+
+          <div className="Contact-Us">
+            <button>Contact us</button>
+          </div>
+        </form>
+      </section>
+    </div>
+  );
+}
+
+function KnowLanguagesTable() {
+  return (
+    <table border={1}>
+      <tr>
+        <th>Language</th>
+        <th>Level</th>
+      </tr>
+      <tr>
+        <td>Romana</td>
+        <td>Native-Language</td>
+      </tr>
+      <tr>
+        <td>English</td>
+        <td>B2</td>
+      </tr>
+      <tr>
+        <td>Deutschland</td>
+        <td>B2</td>
+      </tr>
+    </table>
   );
 }
 
