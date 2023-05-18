755280014c43fbf451bdb9948006c8ff312e7bd4
Vaida Aurel <Vaida_Aurel@yahoo.com>
2023-05-18 22:32:07 +0300

    move AppHeader in header/index

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
-import selfie from "./Pictures/2.jpg";
 import { AppFooter } from "./footer/components";
-import MainMenu from "./menu/MainMenu";
-
-function AppHeader() {
-  return (
-    <header>
-      <div id="header-wrapper">
-        <div>
-          <img src={selfie} />
-        </div>
-        <div id="header-info">
-          <h1>Vaida Aurel Nicolae</h1>
-          <h2>
-            Student:
-            <a target="_blank" href="https://fasttrackit.org/">
-              {" "}
-              FastTrackIt
-            </a>
-          </h2>
-        </div>
-      </div>
-
-      <MainMenu />
-    </header>
-  );
-}
+import AppHeader from "./header";
 
 function ContentWrapper() {
   return (
