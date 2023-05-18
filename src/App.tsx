70b290d7dc8fe7077b5d3068f384f5df875b50e9
Vaida Aurel <Vaida_Aurel@yahoo.com>
2023-05-18 22:20:58 +0300

    add MainMenu function component

--- src/App.tsx
+++ src/App.tsx
@@ -21,30 +21,36 @@ function AppHeader() {
         </div>
       </div>
 
-      <ul id="top-menu-bar">
-        <li>
-          <a href="#" data-page="home">
-            HOME
-          </a>
-        </li>
-        <li>
-          <a href="#" data-page="skills">
-            Skills
-          </a>
-        </li>
-
-        <li>
-          <a href="#" data-page="projects">
-            Projects
-          </a>
-        </li>
-        <li>
-          <a href="#" data-page="languages">
-            Known Languages
-          </a>
-        </li>
-      </ul>
+      <MainMenu />
     </header>
+  );
+}
+
+function MainMenu() {
+  return (
+    <ul id="top-menu-bar">
+      <li>
+        <a href="#" data-page="home">
+          HOME
+        </a>
+      </li>
+      <li>
+        <a href="#" data-page="skills">
+          Skills
+        </a>
+      </li>
+
+      <li>
+        <a href="#" data-page="projects">
+          Projects
+        </a>
+      </li>
+      <li>
+        <a href="#" data-page="languages">
+          Known Languages
+        </a>
+      </li>
+    </ul>
   );
 }
 
