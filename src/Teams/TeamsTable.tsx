7d62b564fe29487aeef01200d3b2a1c6dc31b178
Vaida Aurel <Vaida_Aurel@yahoo.com>
2023-05-25 20:41:24 +0300

    inline teams inside TeamsTable

--- src/Teams/TeamsTable.tsx
+++ src/Teams/TeamsTable.tsx
@@ -1,4 +1,41 @@
 import "./style.css";
+const teams = [
+  {
+    id: "toze8j1610313009673",
+    promotion: "html",
+    members: "Nicolae Matei, HTML",
+    name: "Web Presentation",
+    url: "https://github.com/nmatei/web-intro-presentation"
+  },
+  {
+    id: "ezabnf1630345987541",
+    promotion: "css",
+    members: "Nicolae",
+    name: "Names",
+    url: "https://github.com/nmatei/nmatei.github.io"
+  },
+  {
+    id: "86mq81630347385708",
+    promotion: "js",
+    members: "Matei, Andrei",
+    name: "JS/HTML/CSS Quiz",
+    url: "https://github.com/nmatei/simple-quiz-app"
+  },
+  {
+    id: "w2aal1630347411901",
+    promotion: "js",
+    members: "FastTrackIT Students, Nicolae",
+    name: "Teams Networking",
+    url: "https://github.com/nmatei/teams-networking"
+  },
+  {
+    id: "w2aal1630347411902",
+    promotion: "react",
+    members: "FastTrackIT Students, Nicolae",
+    name: "Teams Networking React",
+    url: "https://github.com/nmatei/teams-networking-react"
+  }
+];
 
 export function TeamsTable() {
   return (
@@ -13,7 +50,24 @@ export function TeamsTable() {
             <th></th>
           </tr>
         </thead>
-
+        <tbody>
+          {teams.map(({ promotion, members, name, url, id }) => (
+            <tr key={id}>
+              <td>{promotion}</td>
+              <td>{members}</td>
+              <td>{name}</td>
+              <td>
+                <a href={url} target="_blank">
+                  {url.replace("https://github.com", "")}
+                </a>
+              </td>
+              <td>
+                <a className="removeBtn">&#10006</a>
+                <a className="edit-btn">&#9998</a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
         <tfoot>
           <tr>
             <td>
